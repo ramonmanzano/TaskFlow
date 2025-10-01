9dce53b7633c12572c8815e06519f159285a21b9
soyramonmanzano <soyramonmanzano@gmail.com>
2025-10-01 12:20:11 +0200

    ultimo ejercicio

--- public/index.php
+++ public/index.php
@@ -1,16 +1,25 @@
+<?php
+  // Bloque de configuración principal
+$pageTitle = SITE_NAME
+define("SITE_NAME", "TaskFlow");
+$userName = "Ramon"; 
+$userAge = 20;             
+$isPremiumUser = false;     
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Página de inicio</title>
-	<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle; ?></title>
+	
 </head>
 <body>
-	<h1>Bienvenido a mi sitio web</h1>
-	<?php
-		echo "Hola desde PHP a mi pagina de Ramon";
-	?>
+	<main>
+        <h2>Perfil del Usuario</h2>
+        <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
+        <p><strong>Edad:</strong> <?php echo $userAge; ?> años</p>
+        <p><strong>Estado de la cuenta:</strong> Usuario <?php echo $isPremiumUser ? "Premium" : "Estándar"; ?></p>
+    </main>
 </body>
 </html>
